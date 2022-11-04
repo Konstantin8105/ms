5e4c8ca4733e1820febd091172aaa70336ae118d
Konstantin8105 <konstantin8105@gmail.com>
2022-11-04 21:52:11 +0300

    proto: prepare

--- protoGL.go
+++ protoGL.go
@@ -18,8 +18,6 @@ func init() {
 	runtime.LockOSThread()
 }
 
-const fontSize = int32(16)
-
 func main() {
 	if err := glfw.Init(); err != nil {
 		err = fmt.Errorf("failed to initialize glfw: %v", err)
@@ -51,6 +49,7 @@ func main() {
 	if err != nil {
 		return
 	}
+	const fontSize = int32(16)
 	font, err := gltext.LoadTruetype(fd, fontSize, 32, 127, gltext.LeftToRight)
 	if err != nil {
 		return
@@ -61,36 +60,107 @@ func main() {
 	}
 	// font is prepared
 
+	color := func(c tcell.Color) (R, G, B float32) {
+		switch c {
+		case tcell.ColorBlack:
+			R, G, B = 0, 0, 0
+		case tcell.ColorWhite:
+			R, G, B = 1, 1, 1
+		case tcell.ColorYellow:
+			R, G, B = 1, 1, 0
+		case tcell.ColorViolet:
+			R, G, B = 0.5, 0, 1.0
+		case tcell.ColorMaroon:
+			R, G, B = 0.5, 0, 0
+		default:
+			panic(c)
+		}
+		return
+	}
+
 	// DrawText text on the screen
-	DrawText := func(cell vl.Cell, x, y int32) {
+	DrawText := func(cell vl.Cell, x, y int) {
 
 		// We need to offset each string by the height of the
 		// font. To ensure they don't overlap each other.
-		//	_, h := font.GlyphBounds()
+		gw, gh := font.GlyphBounds()
 
-		// Draw a rectangular backdrop using the string's metrics.
-		//sw, sh := font.Metrics(string(cell.R))
-		//_ = sw
-		//_ = sh
-		//gl.Color4ub(uint8(x%255), uint8(y%255), 1, 122)
-		//var x1, y1, x2, y2 float32 = float32(x), float32(y), float32(x + int32(sw)), float32(y + int32(sh))
-		//gl.Rectf(x1/600.,y1/800., x2/600., y2/800.)
-		//fmt.Println(x, y, sw, sh, ">>>>", x1, y1, x2, y2)
+		x *= gw
+		y *= gh
 
-		// Render the string.
-		// gl.Color4f(255, 1, 1, 255)
-		// err := font.Printf(float32(x), float32(y), string(cell.R))
-		// if err != nil {
-		// 	panic(err)
-		// 	// return err
-		// }
+		// prepare colors
+		fg, bg, attr := cell.S.Decompose()
+		_ = fg
+		_ = bg
+		_ = attr
 
-		gl.Color4ub(255, 0, 0, 255)
+		// rectangle
+		var vp [4]int32
+		gl.GetIntegerv(gl.VIEWPORT, &vp[0])
+
+		gl.PushAttrib(gl.TRANSFORM_BIT)
+		gl.MatrixMode(gl.PROJECTION)
+		gl.PushMatrix()
 		gl.LoadIdentity()
-		err := font.Printf(float32(x), float32(y), string(cell.R))
+		gl.Ortho(float64(vp[0]), float64(vp[2]), float64(vp[1]), float64(vp[3]), 0, 1)
+		gl.PopAttrib()
+
+		gl.PushAttrib(gl.LIST_BIT | gl.CURRENT_BIT | gl.ENABLE_BIT | gl.TRANSFORM_BIT)
+		{
+			gl.MatrixMode(gl.MODELVIEW)
+			gl.Disable(gl.LIGHTING)
+			gl.Disable(gl.DEPTH_TEST)
+			gl.Enable(gl.BLEND)
+			gl.Enable(gl.TEXTURE_2D)
+
+			gl.BlendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA)
+			gl.TexEnvf(gl.TEXTURE_ENV, gl.TEXTURE_ENV_MODE, gl.MODULATE)
+			//gl.BindTexture(gl.TEXTURE_2D, f.texture)
+			//gl.ListBase(f.listbase)
+
+			var mv [16]float32
+			gl.GetFloatv(gl.MODELVIEW_MATRIX, &mv[0])
+			gl.PushMatrix()
+			{
+				gl.LoadIdentity()
+
+				//mgw := float32(gw) // f.maxGlyphWidth)
+				mgh := float32(gh) // f.maxGlyphHeight)
+
+				//switch f.config.Dir {
+				//case LeftToRight, TopToBottom:
+				gl.Translatef(float32(x), float32(vp[3])-float32(y)-mgh, 0)
+				//case RightToLeft:
+				//	gl.Translatef(x-mgw, float32(vp[3])-y-mgh, 0)
+				//}
+
+				//fmt.Println(cell.S)
+				r, g, b := color(bg)
+				gl.Color4f(r, g, b, 1)
+
+				gl.Rectf(0, 0, float32(gw), float32(gh))
+
+				gl.MultMatrixf(&mv[0])
+				//gl.CallLists(int32(len(indices)), gl.UNSIGNED_INT, unsafe.Pointer(&indices[0]))
+			}
+			gl.PopMatrix()
+		}
+		gl.PopAttrib()
+
+		gl.PushAttrib(gl.TRANSFORM_BIT)
+		gl.MatrixMode(gl.PROJECTION)
+		gl.PopMatrix()
+		gl.PopAttrib()
+
+		gl.LoadIdentity()
+
+		str := string(cell.R)
+
+		r, g, b := color(fg)
+		gl.Color4f(r, g, b, 1)
+		err = font.Printf(float32(x), float32(y), str)
 		if err != nil {
 			panic(err)
-			// return err
 		}
 	}
 
@@ -116,6 +186,32 @@ func main() {
 	var heightSymbol uint
 	var w, h int
 
+	window.SetKeyCallback(func(w *glfw.Window, key glfw.Key, scancode int, action glfw.Action, mods glfw.ModifierKey) {
+		screen.Event(tcell.NewEventKey(tcell.KeyRune, rune('R'), tcell.ModNone)) // TODO add for arrow
+	})
+
+	window.SetScrollCallback(func(w *glfw.Window, xoffset, yoffset float64) {
+		x, y := w.GetCursorPos()
+		gw, gh := font.GlyphBounds()
+		xs := int(x / float64(gw))
+		ys := int(y / float64(gh))
+
+		var bm tcell.ButtonMask
+		if yoffset < 0 {
+			bm = tcell.WheelDown
+		}
+		if 0 < yoffset {
+			bm = tcell.WheelUp
+		}
+		if xoffset < 0 {
+			bm = tcell.WheelLeft
+		}
+		if 0 < xoffset {
+			bm = tcell.WheelRight
+		}
+		screen.Event(tcell.NewEventMouse(xs, ys, bm, tcell.ModNone))
+	})
+
 	window.SetMouseButtonCallback(func(
 		w *glfw.Window,
 		button glfw.MouseButton,
@@ -136,8 +232,11 @@ func main() {
 		}
 		// calculate position
 		x, y := w.GetCursorPos()
-		xs := int(x / float64(fontSize))
-		ys := int(y / float64(fontSize))
+		gw, gh := font.GlyphBounds()
+		xs := int(x / float64(gw))
+		ys := int(y / float64(gh))
+		_ = gh
+		_ = gw
 		// create event
 		switch action {
 		case glfw.Press:
@@ -159,8 +258,12 @@ func main() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		gl.ClearColor(1, 1, 1, 1)
 
-		widthSymbol = uint(w) / uint(fontSize)
-		heightSymbol = uint(h) / uint(fontSize)
+		gl.Color4f(0.9, 0.9, 0.8, 0.9)
+		gl.Rectf(-0.5, -0.5, 0.5, 0.5)
+
+		gw, gh := font.GlyphBounds()
+		widthSymbol = uint(w) / uint(gw)
+		heightSymbol = uint(h) / uint(gh)
 		screen.SetHeight(heightSymbol)
 		screen.GetContents(widthSymbol, &cells)
 		for r := 0; r < len(cells); r++ {
@@ -168,7 +271,7 @@ func main() {
 				continue
 			}
 			for c := 0; c < len(cells[r]); c++ {
-				DrawText(cells[r][c], int32(c)*fontSize, int32(r)*fontSize)
+				DrawText(cells[r][c], c, r)
 			}
 		}
 
