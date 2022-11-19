1c0b0b17947ca88ba469344ca4eb15564ce89d4c
konstantin8105 <konstantin8105@gmail.com>
2022-11-20 02:24:14 +0300

    step

--- demo.go
+++ demo.go
@@ -7,22 +7,20 @@ import (
 	"os"
 
 	"github.com/Konstantin8105/ms"
-	"github.com/Konstantin8105/vl"
-	"github.com/gdamore/tcell/v2"
 )
 
 // gmsh -2 -smooth 10 -format msh22 1.geo
 
 func main() {
-	root, action, err := ms.UserInterface()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v", err)
-		os.Exit(1)
-	}
-	err = vl.Run(root, action, nil, tcell.KeyCtrlC)
-	if err != nil {
-		fmt.Println(ms.PrintInfo())
-	}
+	// root, action, err := ms.UserInterface()
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "%v", err)
+	// 	os.Exit(1)
+	// }
+	// err = vl.Run(root, action, nil, tcell.KeyCtrlC)
+	// if err != nil {
+	// 	fmt.Println(ms.PrintInfo())
+	// }
 	// create a new model
 	if err := ms.Run("testdata/1.geo", nil); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
