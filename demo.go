4b1e17ae1387276d1f99cd44af926ce8c04b59ea
Konstantin8105 <konstantin8105@gmail.com>
2023-08-24 18:03:12 +0300

    demo: first view

--- demo.go
+++ demo.go
@@ -22,7 +22,8 @@ func main() {
 	// 	fmt.Println(ms.PrintInfo())
 	// }
 	// create a new model
-	if err := ms.Run("testdata/1.geo", nil); err != nil {
+	// if err := ms.Run("testdata/1.geo", nil); err != nil {
+	if err := ms.Run("testdata/IntersectionSpiral", nil); err != nil {
 		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(1)
 	}
